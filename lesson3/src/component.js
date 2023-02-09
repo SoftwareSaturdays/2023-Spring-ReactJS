161455dad4e5266de04e3f912b00884b794e7a4e
Adithya Chandrasekar <21148381+adithyachan@users.noreply.github.com>
2023-02-08 19:17:16 -0500

    Updated component template

--- lesson3/src/component.js
+++ lesson3/src/component.js
@@ -1,21 +1,9 @@
 import React from 'react';
 
-class /*FILENAME*/ extends React.Component {
-
-    // Ignore this for now...
-    constructor(props) {
-        super(props)
-    }
-
-    render () {
-        // Type your code here...
-
-
-
-        // Return some JSX here...
-        return null;
-    }
+export function Component() {
+    return (
+        <>
+        {/* your code here */}
+        </>
+    );
 }
-
-
-export default /*FILENAME*/;
