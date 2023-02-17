6ad1f53ad1c61f63fffae1fe86fb17b4956164d0
Adithya Chandrasekar <21148381+adithyachan@users.noreply.github.com>
2023-02-17 12:58:38 -0500

    Converted checkpoints to templates

--- checkpoint/src/components/Checkpoint1.jsx
+++ checkpoint/src/components/Checkpoint1.jsx
@@ -3,9 +3,10 @@ import Card from "./Card";
 
 export default function Checkpoint1() {
   const pokemon = ["bulbasaur", "charmander", "squirtle"]
-  /* For Checkpoint 1: Create a mapper function to map each pokemon to a card component passing in the pokemon as a prop to the card. Call this component with all the pokemon cards, cards.*/
+  /* For Checkpoint 1: Create a mapper function to map each pokemon to a card component passing in the pokemon as a prop to the card. */
 
-  const cards = pokemon.map((name) => <Card pokemon={name} />)
+  // TODO: Create a mapper function to map each pokemon to a card component passing in the pokemon as a prop to the card. Set the cards equal to the mapper's output
+  const cards = []
   
   
   return (
--- checkpoint/src/components/Checkpoint2.jsx
+++ checkpoint/src/components/Checkpoint2.jsx
@@ -4,23 +4,23 @@ import Card from "./Card";
 export default function Checkpoint2() {
     /*For Checkpoint 2, use the given getAPIData function and set up a pokemon useState variable to store the data. Then, like in Demo2, call getAPIData in useEffect to populate the pokmeon array. Lastly, use your code from checkpoint 1 to map each of the pokemon into card elements. Feel free to mess around with stuff if you are done early. */
 
+    // state variable to hold all the mapped cards
     const [pokemonCards, setPokemonCards] = useState(null)
 
     // getAPIData gets the data from the pokeapi, returns an array of pokemon
     const getAPIData = async () => {
-        // This code is provided, it can be complicated
+        /* START OF API CALLING. MODIFY AT YOUR OWN RISK */
         const url = "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0"; // URL of the API
         const response = await fetch(url); // Get the data from the PokeAPI
         const responseJSON = await response.json(); // Turn the data into a JSON object that we can use
 
         // This should seem familiar
         const responsePokemon = responseJSON.results.map((item) => item.name);
+        /* END OF API CALLING. MODIFY AT YOUR OWN RISK */
 
-        // Map the pokemon to cards
-        const cards = responsePokemon.map((name) => <Card pokemon={name} />)
+        // TODO: Map the pokemon to cards
 
-        // Set pokemonCards to the mapped cards 
-        setPokemonCards(cards)
+        // TODO: Set pokemonCards to the mapped cards 
     }
 
     useEffect(() => {
